262a01a6adbd77ecd310678156e7a69628af8181
aston1337 <aston.star1337@gmail.com>
2022-04-12 16:30:31 +0300

    init: start chapret 1. My first calculater

---
+++ chapter_01/Chapter_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter_1 {
+    class Calculator {
+        public static void Main(string[] args) {
+            Calc num = new Calc();
+            int ans = num.add(12, 23);
+            Console.WriteLine($"The result is {ans}");
+            
+        }
+    }
+
+    class Calc {
+        public int add(int x, int y) {
+            return x + y;
+        }
+    }
+}
