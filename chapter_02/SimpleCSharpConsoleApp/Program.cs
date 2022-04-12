e06f5e365ea3bf0c4e9985d5e67300ade5886b3e
aston1337 <aston.star1337@gmail.com>
2022-04-12 22:42:23 +0300

    init: start chapter_2 feat: consoleApp

---
+++ chapter_02/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter_2 {
+    class ConsoleApp {
+        public static void Main(string[] args) {
+            Console.Title = "My rocking app";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("************************");
+            Console.WriteLine("**** My Rocking app ****");
+            Console.WriteLine("************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+        }
+    }
+}
