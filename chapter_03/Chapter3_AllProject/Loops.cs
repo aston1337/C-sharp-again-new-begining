8f8db9c317304d39cc7e70950c73e87d28ec7947
aston1337 <aston.star1337@gmail.com>
2022-04-21 00:22:22 +0300

    feat: add while do-while loops

--- chapter_03/Chapter3_AllProject/Loops.cs
+++ chapter_03/Chapter3_AllProject/Loops.cs
@@ -5,6 +5,7 @@ namespace Chapter_3 {
         public static void iterationsAndDecisions() {
             forLoop();
             forEachLoop();
+            whileDoWhile();
         }
 
         public static void forLoop() {
@@ -16,9 +17,27 @@ namespace Chapter_3 {
         
         public static void forEachLoop() {
             Console.WriteLine("ForEach loop--------------------");
-            for (int i = 1; i <= 10; i++) {
-                Console.WriteLine($"Line number {i}");
+            int[] arr = { 10, 2, 3, 4, 34, 65, 2, 3, 5, };
+            foreach (int i in arr) {
+                Console.Write(i + ", ");
             }
+        }
+        
+        public static void whileDoWhile() {
+            Console.WriteLine("WhileAndDoWhile loops --------------------");
+            string userDone = "";
+            while (userDone.ToLower() != "yes") {
+                Console.WriteLine("In while loop");
+                Console.WriteLine("Are you done? [yes]/[no]");
+                userDone = Console.ReadLine();
+            }
+
+            do {
+                Console.WriteLine("In Do While loop");
+                string userDone2 = "";
+                Console.WriteLine("Are you done? [yes]/[no]");
+                userDone2 = Console.ReadLine();
+            } while (userDone.ToLower() != "yes");
         }
     }
 }
