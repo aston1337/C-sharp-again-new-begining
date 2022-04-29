08a6280557d992d43266c171a97939ef7d96e5e9
aston1337 <aston.star1337@gmail.com>
2022-04-29 16:01:44 +0300

    add Arrays as Args

---
+++ chapter_04/chapter_04/ArraysAsArgsAndReturnValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace chapter_04 {
+    public class ArraysAsArgsAndReturnValues {
+        public static void arraysAsArgsAndReturnValues() {
+            int[] myInts = new[] { 1, 4, 5, 6, 2, 3, 6 };
+            printArray(myInts);
+        }
+
+        public static void printArray(int[] myInts) {
+            for (int i = 0; i < myInts.Length; i++) {
+                Console.WriteLine($"Item {i} is {myInts[i]}");
+            }
+        }
+
+        static string[] getStringArray() {                          // string[] потому что возвращаем массив строк
+            string[] theStrings = { "Hello", "Welcome", "Hi" };
+            return theStrings;
+        }
+    }
+}
--- chapter_04/chapter_04/MainMethod.cs
+++ chapter_04/chapter_04/MainMethod.cs
@@ -1,8 +1,21 @@
 ﻿namespace chapter_04 {
     class MainMethod {
+        /*
+         Clear()    This static method sets a range of elements in the array to empty values (0 for
+                    numbers, null for object references, false for Booleans).
+         CopyTo()   This method is used to copy elements from the source array into the destination
+                    array.
+         Length     This property returns the number of items within the array.
+         Rank       This property returns the number of dimensions of the current array.
+         Reverse()  This static method reverses the contents of a one-dimensional array.
+         Sort()     This static method sorts a one-dimensional array of intrinsic types. If the
+                    elements in the array implement the IComparer interface, you can also sort
+                    your custom types (see Chapters 8 and 10).  
+        */   
         public static void Main(string[] args) {
-            Arrays.funWithArrays();
-            MultiDimensionalArrays.callArrays();
+            //Arrays.funWithArrays();
+            //MultiDimensionalArrays.callArrays();
+            //ArraysAsArgsAndReturnValues.arraysAsArgsAndReturnValues();
         }
     }
 }
